9fe7c5bdf5c512a20c4b436d5e453948567e457c
Sylphy-0xd3ac <1393309348@qq.com>
2024-12-11 17:45:08 +0800

    Update reload.ts

--- src/command/exec-chat/root/reload.ts
+++ src/command/exec-chat/root/reload.ts
@@ -109,7 +109,7 @@ export async function listModulesVersion(hazel, core, hold, socket, line) {
 
   // 遍历版本列表
   versions.forEach((version) => {
-    versionList += "[" + (versions.indexOf(version) + 1) + "] " + version + (hazel.loadHistory.get(modulePath)[hazel.loadHistory.get(modulePath).length - 1] === version ? " (最新)" : "") + "\n";
+    versionList += "[" + (versions.indexOf(version) + 1) + "] " + version + (hazel.loadHistory.get(modulePath)[hazel.loadHistory.get(modulePath).length - 1] === version ? " (当前版本)" : "") + "\n";
   });
 
   // 发送版本列表
