b076782bfc5406a355026115dbf5c4609dcbe225
Sylphy-0xd3ac <Sylphy-0xd3ac@qq.com>
2024-12-27 18:23:31 +0800

    None.

--- hazel/recursive-readdir.ts
+++ hazel/recursive-readdir.ts
@@ -2,19 +2,16 @@ import { readdirSync } from "node:fs";
 import { join } from "node:path";
 
 function readDir(baseDir, resultArray) {
-  // 获取当前执行目录
-  const isNexe = (global.__nexe || false);
-  const basePath = isNexe ? process.cwd() : baseDir;
-  let dirResult = readdirSync(basePath, {
+  let dirResult = readdirSync(baseDir, {
     encoding: "utf-8",
     withFileTypes: true,
   });
 
   dirResult.forEach((value) => {
     if (!value.isDirectory()) {
-      resultArray.push(join(basePath, value.name));
+      resultArray.push(join(baseDir, value.name));
     } else {
-      resultArray = readDir(join(basePath, value.name), resultArray);
+      resultArray = readDir(join(baseDir, value.name), resultArray);
     }
   });
 
