9e26e5a8aab5c81504f0b0f4f6dd7cfdb62b1e40
Sylphy-0xd3ac <Sylphy-0xd3ac@qq.com>
2025-01-04 16:34:41 +0800

    None.

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,12 +1,12 @@
-import { build } from 'esbuild';
-import { sync } from 'glob';
-import { copyFileSync,mkdirSync,readFileSync,writeFileSync } from 'fs';
-import { load,dump } from "js-yaml";
+import { build } from "esbuild";
+import { sync } from "glob";
+import { copyFileSync, mkdirSync, readFileSync, writeFileSync } from "fs";
+import { load, dump } from "js-yaml";
 
 let globStartTimestamp = Date.now();
 // 使用glob获取当前目录及子目录下的所有ts文件
-const tsFiles = sync('**/*.ts', {
-  ignore: ['node_modules/**', 'dist/**'], // 忽略node_modules和dist目录
+const tsFiles = sync("**/*.ts", {
+  ignore: ["node_modules/**", "dist/**"], // 忽略node_modules和dist目录
 });
 console.log(`Glob finished in ${Date.now() - globStartTimestamp}ms`);
 
@@ -14,31 +14,37 @@ console.log(`Glob finished in ${Date.now() - globStartTimestamp}ms`);
 const buildOptions = {
   entryPoints: tsFiles,
   bundle: false,
-  target: 'esnext',
-  format: 'esm',
-  platform: 'node',
-  outdir: 'dist', // 输出目录
+  target: "esnext",
+  format: "esm",
+  platform: "node",
+  outdir: "dist", // 输出目录
 };
 
 let buildStartTimestamp = Date.now();
 // 执行构建
-build(buildOptions).then(() => {
-  // 创建dist/config目录
-  mkdirSync('dist/config', { recursive: true });
+build(buildOptions)
+  .then(() => {
+    // 创建dist/config目录
+    mkdirSync("dist/config", { recursive: true });
 
-  // 拷贝config
-  copyFileSync('config/config.yml', 'dist/config/config.yml');
-  copyFileSync('config/allow.txt', 'dist/config/allow.txt');
+    // 拷贝config
+    copyFileSync("config/config.yml", "dist/config/config.yml");
+    copyFileSync("config/allow.txt", "dist/config/allow.txt");
 
-  // 拷贝mainConfig
-  copyFileSync('config.yml', 'dist/config.yml');
+    // 拷贝mainConfig
+    copyFileSync("config.yml", "dist/config.yml");
 
-  let config = load(readFileSync("./config.yml", { encoding: "utf-8", flag: "r" }));
-  config.hazel.moduleDirs.staticDir = "/boot/boot.js,/boot/boot_ws.js";
-  writeFileSync("./dist/config.yml", dump(config, 'dist/config.yml') , { encoding: "utf-8" });
-}).catch((err) => {
-  console.error(err);
-  process.exit(1);
-});
+    let config = load(
+      readFileSync("./config.yml", { encoding: "utf-8", flag: "r" }),
+    );
+    config.hazel.moduleDirs.staticDir = "/boot/boot.js,/boot/boot_ws.js";
+    writeFileSync("./dist/config.yml", dump(config, "dist/config.yml"), {
+      encoding: "utf-8",
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
 
 console.log(`Build finished in ${Date.now() - buildStartTimestamp}ms`);
