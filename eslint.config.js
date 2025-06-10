7e327f4d46cf5eacaf69fc3f32de721bf760109c
Sylphy-0xd3ac <Sylphy@240729.xyz>
2025-06-10 18:34:47 +0800

    build: 更新 eslint.config.js，新增对 dist/** 文件夹的忽略

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ import tsParser from "@typescript-eslint/parser";
 export default [
   {
     files: ["**/*.{js,mjs,cjs,ts,tsx}"],
-    ignores: ["node_modules/**", "eslint.config.js"], // 忽略特定文件夹
+    ignores: ["node_modules/**", "eslint.config.js", "dist/**"], // 忽略特定文件
     languageOptions: {
       parser: tsParser, // 使用 @typescript-eslint/parser 作为解析器
       parserOptions: {
